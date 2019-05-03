ba064c373d045fddd81a9c8d908d66389ce16d17
DLeasure <DLeasure9@gmail.com>
2019-05-02 21:55:54 -0400

    first commit

---
+++ JavaScript-Game-Lab-Part-1.js
@@ -0,0 +1,28 @@
+let userHealth = 40;
+let grantHealth = 10;
+let userWins = 0;
+const likeToPlay = prompt("Would you like to play a game?");
+// const likeToPlay = "yes";
+// const userName = "Dave";
+
+if (likeToPlay === "yes" || likeToPlay === "Yes") {
+    userName = prompt("Name your character");
+}
+
+while (userWins < 3 && userHealth > 0) {
+    const healthDecrement = (Math.floor(Math.random() * 2) + 1);
+    userHealth -= healthDecrement;
+    grantHealth -= healthDecrement;
+    console.log(`Health: ${userName} ${userHealth} - ${grantHealth} Grant`);
+    if (grantHealth < 1) {
+        userWins++;
+        console.log(`${userName} has won game ${userWins}!`);
+        grantHealth = 10;
+    }
+    if (userHealth < 1) {
+        console.log(`${userName} has lost the game!`);
+    }
+    if (userWins === 3) {
+        console.log(`${userName} has defeated Grant!`);
+    }
+}
